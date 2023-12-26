37b5bd95914685411c6eb45333186fc1beae0ab8
manoj <manojmahanta1998@gmail.com>
2023-12-26 21:53:12 +0530

    initial commit

---
+++ src/main/java/com/global/groupware/employeewise/EmployeeWiseApplication.java
@@ -0,0 +1,13 @@
+package com.global.groupware.employeewise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeWiseApplication.class, args);
+	}
+
+}
