764d2379c7480753dbc5bcee1db99d00e18d6e5c
manoj <manojmahanta1998@gmail.com>
2023-12-27 02:23:12 +0530

    initial commit

---
+++ src/main/java/com/global/groupware/employeewise/services/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.global.groupware.employeewise.services;
+
+import com.global.groupware.employeewise.entities.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    Employee addEmployee(Employee employee);
+
+    List<Employee> getAllEmployees(int page, int pageSize, String sortBy);
+
+    void deleteEmployeeById(String employeeId);
+
+    Employee updateEmployeeById(String employeeId, Employee updatedEmployee);
+
+    Employee getNthLevelManager(String employeeId, int level);
+}
